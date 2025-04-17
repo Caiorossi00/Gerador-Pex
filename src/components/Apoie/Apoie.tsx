57c85edba09d0d2220305e058cca61c867f15250
Caiorossi00 <caio_rossi56@hotmail.com>
2025-04-17 20:50:19 -0300

    update número de seguidores

--- src/components/Apoie/Apoie.tsx
+++ src/components/Apoie/Apoie.tsx
@@ -29,10 +29,10 @@ const Apoie = () => {
 
       <div className="apoio-resultados">
         <h3>
-          <span>+2 </span>Novos Seguidores
+          <span>+7 </span>Novos Seguidores
         </h3>
         <h3>
-          <span>+2 </span>Doadores
+          <span>+3 </span>Doadores
         </h3>
       </div>
 
