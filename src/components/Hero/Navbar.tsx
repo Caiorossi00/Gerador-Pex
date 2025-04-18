3a68cec06220319ed0364f125b724787fcd6689b
Caiorossi00 <caio_rossi56@hotmail.com>
2025-04-18 08:28:18 -0300

    responsividade navbar

--- src/components/Hero/Navbar.tsx
+++ src/components/Hero/Navbar.tsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import "../../styles/Navbar.scss";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <ul className="nav-links">
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        <span className={isOpen ? "line open" : "line"}></span>
+        <span className={isOpen ? "line open" : "line"}></span>
+        <span className={isOpen ? "line open" : "line"}></span>
+      </div>
+
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li>
-          <a href="#form">Gerar PEX</a>
+          <a href="#form" onClick={() => setIsOpen(false)}>
+            Gerar PEX
+          </a>
         </li>
         <li>
-          <a href="#faq">FAQ</a>
+          <a href="#faq" onClick={() => setIsOpen(false)}>
+            FAQ
+          </a>
         </li>
         <li>
-          <a href="#apoie">Apoie o projeto</a>
+          <a href="#apoie" onClick={() => setIsOpen(false)}>
+            Apoie o projeto
+          </a>
         </li>
-        <li>
-          <button>
+        <li className="hide-on-mobile">
+          <button onClick={() => setIsOpen(false)}>
             <a href="#form">Experimentar</a>
           </button>
         </li>
