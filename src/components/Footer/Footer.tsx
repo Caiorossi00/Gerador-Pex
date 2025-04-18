cdd9d6967154f8adc63b3b3f5d614c84cf02f4a2
Caiorossi00 <caio_rossi56@hotmail.com>
2025-04-18 09:28:30 -0300

    responsividade footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,43 +4,50 @@ import "../../styles/Footer.scss";
 const Footer = () => {
   return (
     <div id="footer">
-      <div className="services-footer">
-        <h1>Serviços</h1>
-        <ul>
-          <li>
-            <a href="#home">Início</a>
-          </li>
-          <li>
-            <a href="#form">Gerar Pex</a>
-          </li>
-          <li>
-            <a href="#faq">FAQ</a>
-          </li>
-          <li>
-            <a href="#apoie">Apoie o Projeto</a>
-          </li>
-        </ul>
+      <div className="footer-content">
+        <div className="services-footer">
+          <h1>Serviços</h1>
+          <ul>
+            <li>
+              <a href="#home">Início</a>
+            </li>
+            <li>
+              <a href="#form">Gerar Pex</a>
+            </li>
+            <li>
+              <a href="#faq">FAQ</a>
+            </li>
+            <li>
+              <a href="#apoie">Apoie o Projeto</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="socials-footer">
+          <h1>Social</h1>
+          <ul>
+            <li>
+              <a
+                href="https://instagram.com/caiorossi.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/caio-rossi-dev/">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://github.com/Caiorossi00">Github</a>
+            </li>
+          </ul>
+        </div>
       </div>
 
-      <div className="socials-footer">
-        <h1>Social</h1>
-        <ul>
-          <li>
-            <a
-              href="https://instagram.com/caiorossi.dev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Instagram
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/caio-rossi-dev/">LinkedIn</a>
-          </li>
-          <li>
-            <a href="https://github.com/Caiorossi00">Github</a>
-          </li>
-        </ul>
+      <div className="footer-credits">
+        <p>Este projeto foi gentilmente desenvolvido por Caio Rossi ♡</p>
+        <p>18/04/2025</p>
       </div>
     </div>
   );
